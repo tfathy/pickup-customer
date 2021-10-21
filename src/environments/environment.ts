62793bb1e0d29d780bc3b713ab0cc1a04efbea80
Tarek Bakr <tarekfathi2003@gmail.com>
2021-10-21 18:45:36 +0300

    deploy android version 15.5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  backEndApiRoot: 'http://localhost:802',
+  backEndApiRoot:'https://tarek-bakr.net:802',
   facebookGraphQl:
     'https://graph.facebook.com/me?fields=id,name,picture.width(720),email,gender,link&access_token=',
   googleMapsAPIKey: 'AIzaSyBifcwaKgvMIfLesKQBEvW_81vrqXewUMc',
